1c2d5d85f6de036f1a37b73bf2c1f84f1e38fad3
Franklin Liu <16155488+liufran1@users.noreply.github.com>
2023-05-31 17:18:53 -0400

    add review summarizer page

---
+++ load_reviews.js
@@ -0,0 +1,44 @@
+function loadWebsite() {
+    var urlInput = document.getElementById("urlInput");
+    var url = urlInput.value;
+    var reviewText = `Getting reviews for ${url}`
+
+    var previewContainer = document.getElementById("previewContainer");
+    previewContainer.innerHTML = `<iframe src="${url}"></iframe>`;
+
+
+    var outputContainer = document.getElementById("reviewSummary");
+    outputContainer.innerHTML = `
+        <div class="progress-bar">
+          <div class="progress"></div>
+        </div>
+        <div class="spinning-wheel"></div>
+      `;
+
+    var progressBar = document.querySelector(".progress-bar .progress");
+    var spinningWheel = document.querySelector(".spinning-wheel");
+
+      // Set a timeout to show the spinning wheel after 10 seconds
+    var timeoutId = setTimeout(() => {
+        progressBar.style.width = "100%";
+        progressBar.style.transition = "";
+        spinningWheel.style.display = "inline-block";
+      }, 10000);
+
+      // Make an API call to retrieve the data asynchronously
+      // fetch("https://restaurantreviewsummaries--franklinliu5.repl.co/reviews?input_url" + encodeURIComponent(url))  // replace with custom url
+    
+      fetch("https://run.mocky.io/v3/5ffdb194-a376-4451-9ed3-4ab18b3ca074")
+        .then(response => response.text())
+        .then(data => {
+          outputContainer.innerHTML = `<p align="left">${data}</p>`;
+        })
+        .catch(error => {
+          outputContainer.innerHTML = "Error occurred while fetching data.";
+          console.error(error);
+        });
+
+
+      var reviewSummary = document.getElementById("reviewSummary");
+      reviewSummary.innerHTML = `<p>${reviewText}</p>`;
+    }

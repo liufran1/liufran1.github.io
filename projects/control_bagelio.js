711222f4f9690823da6264934a0f1170ca941deb
liufran1 <16155488+liufran1@users.noreply.github.com>
2023-07-13 00:28:55 +0000

    rebrand to bageld

--- projects/control_bagelio.js
+++ projects/control_bagelio.js
@@ -8,183 +8,183 @@ let num_guesses = 0
 // console.log(num_guesses)
 
 function hashAnswer(string) {
-    var hash = 0;
-    if (string.length == 0) return hash;
-    for (x = 0; x <string.length; x++) {
+  var hash = 0;
+  if (string.length == 0) return hash;
+  for (x = 0; x < string.length; x++) {
     ch = string.charCodeAt(x);
-            hash = ((hash <<5) - hash) + ch;
-            hash = hash & hash;
-        }
-    return hash;
+    hash = ((hash << 5) - hash) + ch;
+    hash = hash & hash;
+  }
+  return hash;
 }
 
 function populateDropdown(atp_wta) {
-    var select = document.getElementById("selectPlayer");
+  var select = document.getElementById("selectPlayer");
 
-    // Hardcode these lists to start
-    var atp_list = ['Carlos Alcaraz',
-     'Novak Djokovic',
-     'Daniil Medvedev',
-     'Casper Ruud',
-     'Stefanos Tsitsipas',
-     'Holger Rune',
-     'Andrey Rublev',
-     'Jannik Sinner',
-     'Taylor Fritz',
-     'Frances Tiafoe',
-     'Karen Khachanov',
-     'Aliassime Auger',
-     'Cameron Norrie',
-     'Borna Coric',
-     'Tommy Paul',
-     'Lorenzo Musetti',
-     'Minaur De',
-     'Hubert Hurkacz',
-     'Francisco Cerundolo',
-     'Pablo Carreno-Busta',
-     'Alexander Zverev',
-     'Jan-Lennard Struff',
-     'Roberto Bautista-Agut',
-     'Grigor Dimitrov',
-     'Sebastian Korda',
-     'Alexander Bublik',
-     'Yoshihito Nishioka',
-     'Nicolas Jarry',
-     'Denis Shapovalov',
-     'Daniel Evans',
-     'Tallon Griekspoor',
-     'Tomas Etcheverry',
-     'Nick Kyrgios',
-     'Fokina Davidovich',
-     'Adrian Mannarino',
-     'Ben Shelton',
-     'Jiri Lehecka',
-     'Matteo Berrettini',
-     'Ugo Humbert',
-     'Andy Murray',
-     'Miomir Kecmanovic',
-     'Lorenzo Sonego',
-     'Christopher Eubanks',
-     'De Van',
-     'Yannick Hanfmann',
-     'Sebastian Baez',
-     'Emil Ruusuvuori',
-     'Jeffrey Wolf',
-     'Gregoire Barrere',
-     'Aslan Karatsev',
-     'Anja Stankovic',
-     'Yulia Starodubtseva',
-     'Wild Seyboth',
-     'Goncalo Oliveira',
-     'Darja Semenistaja',
-     'Antoine Hoang',
-     'Dominic Thiem',
-     'Francesco Passaro',
-     'Nagi Hanatani',
-     'Reka-Luca Jani'];
+  // Hardcode these lists to start
+  var atp_list = ['Carlos Alcaraz',
+    'Novak Djokovic',
+    'Daniil Medvedev',
+    'Casper Ruud',
+    'Stefanos Tsitsipas',
+    'Holger Rune',
+    'Andrey Rublev',
+    'Jannik Sinner',
+    'Taylor Fritz',
+    'Frances Tiafoe',
+    'Karen Khachanov',
+    'Aliassime Auger',
+    'Cameron Norrie',
+    'Borna Coric',
+    'Tommy Paul',
+    'Lorenzo Musetti',
+    'Minaur De',
+    'Hubert Hurkacz',
+    'Francisco Cerundolo',
+    'Pablo Carreno-Busta',
+    'Alexander Zverev',
+    'Jan-Lennard Struff',
+    'Roberto Bautista-Agut',
+    'Grigor Dimitrov',
+    'Sebastian Korda',
+    'Alexander Bublik',
+    'Yoshihito Nishioka',
+    'Nicolas Jarry',
+    'Denis Shapovalov',
+    'Daniel Evans',
+    'Tallon Griekspoor',
+    'Tomas Etcheverry',
+    'Nick Kyrgios',
+    'Fokina Davidovich',
+    'Adrian Mannarino',
+    'Ben Shelton',
+    'Jiri Lehecka',
+    'Matteo Berrettini',
+    'Ugo Humbert',
+    'Andy Murray',
+    'Miomir Kecmanovic',
+    'Lorenzo Sonego',
+    'Christopher Eubanks',
+    'De Van',
+    'Yannick Hanfmann',
+    'Sebastian Baez',
+    'Emil Ruusuvuori',
+    'Jeffrey Wolf',
+    'Gregoire Barrere',
+    'Aslan Karatsev',
+    'Anja Stankovic',
+    'Yulia Starodubtseva',
+    'Wild Seyboth',
+    'Goncalo Oliveira',
+    'Darja Semenistaja',
+    'Antoine Hoang',
+    'Dominic Thiem',
+    'Francesco Passaro',
+    'Nagi Hanatani',
+    'Reka-Luca Jani'];
 
-    wta_list = ['Iga Swiatek',
-     'Aryna Sabalenka',
-     'Elena Rybakina',
-     'Jessica Pegula',
-     'Caroline Garcia',
-     'Ons Jabeur',
-     'Cori Gauff',
-     'Maria Sakkari',
-     'Petra Kvitova',
-     'Darya Kasatkina',
-     'Barbora Krejcikova',
-     'Veronika Kudermetova',
-     'Maia Haddad',
-     'Belinda Bencic',
-     'Ludmilla Samsonova',
-     'Karolina Muchova',
-     'Jelena Ostapenko',
-     'Madison Keys',
-     'Karolina Pliskova',
-     'Viktoria Azarenka',
-     'Donna Vekic',
-     'Ekaterina Alexandrova',
-     'Anastasia Potapova',
-     'Magda Linette',
-     'Qinwen Zheng',
-     'Anhelina Kalinina',
-     'Bernarda Pera',
-     'Elise Mertens',
-     'Petra Martic',
-     'Irina Begu',
-     'Mayar Sherif',
-     'Katerina Siniakova',
-     'Marie Bouzkova',
-     'Lin Zhu',
-     'Paula Badosa',
-     'Marta Kostyuk',
-     'Sorana-Mihaela Cirstea',
-     'Shuai Zhang',
-     'Sloane Stephens',
-     'Anna Blinkova',
-     'Varvara Gracheva',
-     'Marketa Vondrousova',
-     'Elisabetta Cocciaretto',
-     'Jasmine Paolini',
-     'Linda Noskova',
-     'Lauren Davis',
-     'Lucia Bronzetti',
-     'Camila Giorgi',
-     'Shelby Rogers',
-     'Bianca Andreescu',
-     'Anja Stankovic',
-     'Yulia Starodubtseva',
-     'Wild Seyboth',
-     'Goncalo Oliveira',
-     'Darja Semenistaja',
-     'Antoine Hoang',
-     'Dominic Thiem',
-     'Francesco Passaro',
-     'Nagi Hanatani',
-     'Reka-Luca Jani'];
+  wta_list = ['Iga Swiatek',
+    'Aryna Sabalenka',
+    'Elena Rybakina',
+    'Jessica Pegula',
+    'Caroline Garcia',
+    'Ons Jabeur',
+    'Cori Gauff',
+    'Maria Sakkari',
+    'Petra Kvitova',
+    'Darya Kasatkina',
+    'Barbora Krejcikova',
+    'Veronika Kudermetova',
+    'Maia Haddad',
+    'Belinda Bencic',
+    'Ludmilla Samsonova',
+    'Karolina Muchova',
+    'Jelena Ostapenko',
+    'Madison Keys',
+    'Karolina Pliskova',
+    'Viktoria Azarenka',
+    'Donna Vekic',
+    'Ekaterina Alexandrova',
+    'Anastasia Potapova',
+    'Magda Linette',
+    'Qinwen Zheng',
+    'Anhelina Kalinina',
+    'Bernarda Pera',
+    'Elise Mertens',
+    'Petra Martic',
+    'Irina Begu',
+    'Mayar Sherif',
+    'Katerina Siniakova',
+    'Marie Bouzkova',
+    'Lin Zhu',
+    'Paula Badosa',
+    'Marta Kostyuk',
+    'Sorana-Mihaela Cirstea',
+    'Shuai Zhang',
+    'Sloane Stephens',
+    'Anna Blinkova',
+    'Varvara Gracheva',
+    'Marketa Vondrousova',
+    'Elisabetta Cocciaretto',
+    'Jasmine Paolini',
+    'Linda Noskova',
+    'Lauren Davis',
+    'Lucia Bronzetti',
+    'Camila Giorgi',
+    'Shelby Rogers',
+    'Bianca Andreescu',
+    'Anja Stankovic',
+    'Yulia Starodubtseva',
+    'Wild Seyboth',
+    'Goncalo Oliveira',
+    'Darja Semenistaja',
+    'Antoine Hoang',
+    'Dominic Thiem',
+    'Francesco Passaro',
+    'Nagi Hanatani',
+    'Reka-Luca Jani'];
 
-    // var atp_wta = 'atp'
+  // var atp_wta = 'atp'
 
-    if (atp_wta == 'atp' ) {
-        var options = atp_list;
-    }
-    else {
-        var options = wta_list;
-    }
+  if (atp_wta == 'atp') {
+    var options = atp_list;
+  }
+  else {
+    var options = wta_list;
+  }
 
-    for(var i = 0; i < options.length; i++) {
-        var opt = options[i];
-        
-        var el = document.createElement("option");
-        el.textContent = opt;
-        el.value = opt;
-        select.appendChild(el);
-    }
+  for (var i = 0; i < options.length; i++) {
+    var opt = options[i];
+
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    select.appendChild(el);
+  }
 }
 
 function clipboardShare() {
   // Get the text field
   // var copyText = document.getElementById("myInput");
-    if (num_guesses == 4) {
-        var textData = `My bagelio score:  \n${'🟨'.repeat(4)} \n https://liufran1.github.io/projects/bagelio`
+  if (num_guesses == 4) {
+    var textData = `My bageld score:  \n${'🟨'.repeat(4)} \n https://liufran1.github.io/projects/bageld`
 
-    }
-    else {
-        var textData = `My bagelio score:  \n${'🟨'.repeat(num_guesses-1)}🎾${'⬛️'.repeat(4-(num_guesses-1)-1)} \n https://liufran1.github.io/projects/bagelio`
-    }
+  }
+  else {
+    var textData = `My bageld score:  \n${'🟨'.repeat(num_guesses - 1)}🎾${'⬛️'.repeat(4 - (num_guesses - 1) - 1)} \n https://liufran1.github.io/projects/bageld`
+  }
 
-  
+
 
   // Select the text field
   // copyText.select();
   // copyText.setSelectionRange(0, 99999); // For mobile devices
 
-   // Copy the text inside the text field
-  navigator.clipboard.writeText(textData).then(function() {alert("Result copied to clipboard ");});
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(textData).then(function() { alert("Result copied to clipboard "); });
 
   // Alert the copied text
-  
+
 }
 
 populateDropdown(protour)
@@ -196,57 +196,57 @@ var shareButton = "<button onclick=\"clipboardShare()\">Share</button>"
 
 
 function get_guess() {
-    var selectedPlayer = document.getElementById("selectPlayer").value
-    console.log(selectedPlayer)
-    num_guesses +=1;
+  var selectedPlayer = document.getElementById("selectPlayer").value
+  console.log(selectedPlayer)
+  num_guesses += 1;
 
-    if (hashAnswer(selectedPlayer)!=hashedAnswer) {
-        if (num_guesses==4) {
-            intContainer.innerHTML =`<p>Sorry, better luck next time</p><p>Share your results: ${'🟨'.repeat(num_guesses)}</p>`+shareButton
-            progressContainer.innerHTML = ''
-            inputSelector.remove()
-        }
-        else {
-                    // console.log(num_guesses)
-            intContainer.innerHTML = `<img src="../images/bageld/mystery_${num_guesses}.gif" width="100%">`
-            progressContainer.innerHTML = `<p>${'🟨'.repeat(num_guesses)+'⬛️'.repeat(4-(num_guesses))}</p>`
-
-        }
-
-                
+  if (hashAnswer(selectedPlayer) != hashedAnswer) {
+    if (num_guesses == 4) {
+      intContainer.innerHTML = `<p>Sorry, better luck next time</p><p>Share your results: ${'🟨'.repeat(num_guesses)}</p>` + shareButton
+      progressContainer.innerHTML = ''
+      inputSelector.remove()
     }
     else {
-            console.log(num_guesses)
-            intContainer.innerHTML =`<p>You solved it in ${num_guesses} guess${num_guesses > 1?'es':''}</p><p>Share your results: ${'🟨'.repeat(num_guesses-1)}🎾${'⬛️'.repeat(4-(num_guesses-1)-1)}</p>`+shareButton
-            progressContainer.innerHTML = ''
-            inputSelector.remove()
-                // update page for success
-        }
-// 
+      // console.log(num_guesses)
+      intContainer.innerHTML = `<img src="../images/bageld/mystery_${num_guesses}.gif" width="100%">`
+      progressContainer.innerHTML = `<p>${'🟨'.repeat(num_guesses) + '⬛️'.repeat(4 - (num_guesses))}</p>`
+
+    }
 
 
-    // fetch("https://ci39xriub5.execute-api.us-east-2.amazonaws.com/bagelio_check?player_name=" + encodeURIComponent(selectedPlayer)), 
-    // {
-    //  'Access-Control-Allow-Origin':'*'
-    // } 
-    //     .then(response => response.text())
-    //     .then(data => {
-    //       // outputContainer.innerHTML = `<p align="left">${data}</p>`;
-    //      console.log(data)
-    //      if (data=='false') {
-    //          num_guesses +=1;
-    //          console.log(num_guesses)
-    //          intContainer.innerHTML = `<img src="../images/mystery_${num_guesses}.gif" width="100%">`
-    //          // update progress bar
-    //      }
-    //      else {
-    //          console.log(num_guesses)
-    //          // update page for success
-    //      }
-            
-    //     })
-    //     .catch(error => {
-    //       // outputContainer.innerHTML = "We ran into an error while getting reviews, try again in a bit.";
-    //       console.error(error);
-    //     });
+  }
+  else {
+    console.log(num_guesses)
+    intContainer.innerHTML = `<p>You solved it in ${num_guesses} guess${num_guesses > 1 ? 'es' : ''}</p><p>Share your results: ${'🟨'.repeat(num_guesses - 1)}🎾${'⬛️'.repeat(4 - (num_guesses - 1) - 1)}</p>` + shareButton
+    progressContainer.innerHTML = ''
+    inputSelector.remove()
+    // update page for success
+  }
+  // 
+
+
+  // fetch("https://ci39xriub5.execute-api.us-east-2.amazonaws.com/bagelio_check?player_name=" + encodeURIComponent(selectedPlayer)), 
+  // {
+  //  'Access-Control-Allow-Origin':'*'
+  // } 
+  //     .then(response => response.text())
+  //     .then(data => {
+  //       // outputContainer.innerHTML = `<p align="left">${data}</p>`;
+  //      console.log(data)
+  //      if (data=='false') {
+  //          num_guesses +=1;
+  //          console.log(num_guesses)
+  //          intContainer.innerHTML = `<img src="../images/mystery_${num_guesses}.gif" width="100%">`
+  //          // update progress bar
+  //      }
+  //      else {
+  //          console.log(num_guesses)
+  //          // update page for success
+  //      }
+
+  //     })
+  //     .catch(error => {
+  //       // outputContainer.innerHTML = "We ran into an error while getting reviews, try again in a bit.";
+  //       console.error(error);
+  //     });
 }
